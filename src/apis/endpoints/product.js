555d5616c72316f555c80f8f7546549fab53587f
elikese <elikese@gmail.com>
2026-03-11 21:42:14 +0900

    feat: submenu 클릭시 백엔드 연동

--- src/apis/endpoints/product.js
+++ src/apis/endpoints/product.js
@@ -4,3 +4,10 @@ export const addProductAPI = async (dto) => {
   const { data } = await instance.post("/product", dto);
   return data;
 };
+
+export const getSubMenuAPI = async (categoryDetailId, page) => {
+  const { data } = await instance.get(`/product/detail/${categoryDetailId}`, {
+    params: { page },
+  });
+  return data;
+};
