f133619e794d8ccc3b7897e97143cdd27fe3ab1d
yang jun woo <yang jun woo@20251435-p01.unist.ac.kr>
2026-03-13 21:07:35 +0900

    리뷰/상품목록

--- src/apis/endpoints/review.js
+++ src/apis/endpoints/review.js
@@ -1,15 +1,18 @@
 import instance from "../instance";
 
+// 1. 리뷰작성할 수 있는 상품목록 가져오기
 export const getReviewableItemsAPI = async () => {
   const res = await instance.get("/api/reviews/me/items");
   return res.data;
 };
 
+// 2. 리뷰를 등록하도록 하는 API
 export const postReviewAPI = async ({ orderItemId, content }) => {
   const res = await instance.post("/api/reviews", { orderItemId, content });
   return res.data;
 };
 
+// 3. 내가 작성한 리뷰목록 조회하기
 export const getMyReviewsAPI = async () => {
   const res = await instance.get("/api/reviews/me");
   return res.data;
